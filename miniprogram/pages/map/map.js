9d0a721fc670b30e586d961d5eb956d08e1314c8
ppengxiang87 <77740227@qq.com>
2020-03-30 23:55:45 +0800

    0330

---
+++ miniprogram/pages/map/map.js
@@ -0,0 +1,95 @@
+Page({
+  data: {
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: 'T.I.T 创意园'
+    }],
+    covers: [{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '/images/my-location.png'
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '/images/my-location.png'
+    }],
+    i:1
+  },
+  onReady: function(e) {
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+  getCenterLocation: function() {
+    this.mapCtx.getCenterLocation({
+      success: function(res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+  moveToLocation: function() {
+    this.mapCtx.moveToLocation()
+  },
+  translateMarker: function() {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
+      }
+    })
+  },
+  includePoints: function() {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      }, {
+        latitude: 23.00229,
+        longitude: 113.3345211,
+      }]
+    })
+  },
+
+  onLoad() {
+    this.setData({
+      search: this.search.bind(this)
+    })
+  },
+
+  search: function(value) {
+    return new Promise((resolve, reject) => {
+      if (this.data.i % 2 === 0) {
+        setTimeout(() => {
+          resolve([{
+            text: '搜索结果',
+            value: 1
+          }, {
+            text: '搜索结果2',
+            value: 2
+          }])
+        }, 200)
+      } else {
+        setTimeout(() => {
+          resolve([])
+        }, 200)
+
+      }
+      this.setData({
+        i: this.data.i + 1
+      })
+    })
+  },
+  selectResult: function(e) {
+    console.log('select result', e.detail)
+  },
+})
