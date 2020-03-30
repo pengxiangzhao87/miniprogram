dfb11cb97b99dd0ea7fc9ee36d0619f4acfc9cfe
ppengxiang87 <77740227@qq.com>
2020-03-30 23:45:17 +0800

    0330 订单提交；会话

---
+++ miniprogram/pages/classify/classify.js
@@ -0,0 +1,35 @@
+
+Page({
+  data:{
+    orderTypeList:[]
+  },
+
+  onLoad: function () {
+    var _this = this;
+    var db = wx.cloud.database();
+    db.collection('order_type').get({
+      success:function(res){
+        _this.setData({
+          orderTypeList:res.data
+        })
+      }
+    })
+  },
+
+  clickRow:function(e){
+    var _this = this;
+    var index = e.currentTarget.dataset.index;
+    var orderType = _this.data.orderTypeList[index]
+    //获取上一级页面
+    var pages = getCurrentPages();
+    var prevPage = pages[pages.length - 2];
+    // 将我们想要传递的参数在这里直接setData。上个页面就会执行这里的操作。
+    prevPage.setData({ 
+      orderType: orderType
+    })
+    wx.navigateBack({
+      delta: 1  // 返回上一级页面。
+    })
+
+  }
+})
